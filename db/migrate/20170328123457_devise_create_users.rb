c6e9d1edeb61f22facc9357e0e85103de3bb50bb
Okan Binli <okanb3@gmail.com>
2017-03-28 15:39:59 +0300

    user specific fields added to user migration file

--- db/migrate/20170328123457_devise_create_users.rb
+++ db/migrate/20170328123457_devise_create_users.rb
@@ -30,6 +30,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      ## Other Fields
+      t.string :name
+      t.string :address
+      t.string :phone
+
 
       t.timestamps null: false
     end
